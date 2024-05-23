12514c438bf7463300571efb791aa9e12ea4363e
bakhtee <bahtiyorjon.arslonbekov@stud.th-deg.de>
2024-05-23 22:51:35 +0200

    implement sqrt

--- easy/Bakhtee/sqrt/main.py
+++ easy/Bakhtee/sqrt/main.py
@@ -1,2 +1,12 @@
-def sqrt(num):
-    print(int(Math.sqrt(num)))
+
+def mySqrt(x: int) -> int:
+    sqrt = 0
+    if x <= 1:
+        return x
+
+    for i in range((x//2)+1):
+        if i*i <= x:
+            sqrt = i
+        else:
+            break
+    return sqrt
