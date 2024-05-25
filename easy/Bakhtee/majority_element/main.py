275a99bbf1a60546afe2dedef6602ff5fa754114
bakhtee <bahtiyorjon.arslonbekov@stud.th-deg.de>
2024-05-25 07:10:03 +0200

    implement majorityElement function

--- easy/Bakhtee/majority_element/main.py
+++ easy/Bakhtee/majority_element/main.py
@@ -1,11 +1,12 @@
 def majorityElement(nums) -> int:
     nums_set = list(set(nums))
     max_num = 0
+    majority_element = nums[0]
     for num in nums_set:
         if nums.count(num) > max_num:
-            print(num, "worked")
-            max_num = num
-    return max_num
+            max_num = nums.count(num)
+            majority_element = num
+    return majority_element
 
 
 # print(majorityElement([2, 3, 2]))
