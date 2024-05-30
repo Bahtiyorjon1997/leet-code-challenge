0664b6612470ed73a30c544e97a0e9da63c73043
bakhtee <bahtiyorjon.arslonbekov@stud.th-deg.de>
2024-05-30 14:54:44 +0200

    define is same tree function

---
+++ easy/Bakhtee/is_same_tree/main.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p, q) -> bool:
+        if p == None and q == None:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
